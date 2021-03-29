12bf140a9424c1ebb3d6e820ee17e548cdc2894e
Harsh Pawar <pawarharsh21@gmail.com>
2021-03-29 13:42:24 +0530

    add router and app component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import CartState from "./context/cart-context/CartState";
+import UserState from "./context/user-context/UserState";
+import ProductState from './context/product/ProductState';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import SignIn from './components/signin/SignIn';
+import SignUp from './components/sign-up/SignUp';
+import ForgotPassword from './components/forgot-password/ForgotPassword';
+import ResetPassword from './components/reset-password/ResetPassword';
+import HeaderRoutes from './components/HeaderRoutes/HeaderRoutes';
+import PageNotFound from './components/PageNotFound/PageNotFound';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={true}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover />
+
+
+      <BrowserRouter>
+        <UserState>
+          <CartState>
+            <ProductState>
+              <Switch>
+                <Route path="/sign-in" component={SignIn} />
+                <Route path="/sign-up" component={SignUp} />
+                <Route path="/forgot-password" component={ForgotPassword} />
+                <Route path="/reset-password/:resetToken" component={ResetPassword} />
+                <Route path='/404' component={PageNotFound} />
+                <Route path="/" component={HeaderRoutes} />
+              </Switch>
+            </ProductState>
+          </CartState>
+        </UserState>
+      </BrowserRouter>
+
+
+    </div >
   );
 }
 
